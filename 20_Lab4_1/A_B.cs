071f8463e7b3aaad312993f923705c8ad85a6994
PoteznaSowa <64582266+PoteznaSowa@users.noreply.github.com>
2023-08-01 17:41:10 +0300

    Update A_B.cs

--- 20_Lab4_1/A_B.cs
+++ 20_Lab4_1/A_B.cs
@@ -1,7 +1,6 @@
 ﻿namespace _20_Lab4_1 {
 	class A_B : A {
 		public override void DebugInfo(params object[] values) {
-			// Використання різних кольорів у Debug неможливе.
 			base.DebugInfo(values);
 		}
 	}
