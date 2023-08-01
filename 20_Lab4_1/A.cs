0f3b951f26929c19b6c1a6710667bc9dfd3ba0b3
PoteznaSowa <64582266+PoteznaSowa@users.noreply.github.com>
2023-08-01 17:40:55 +0300

    Update A.cs

--- 20_Lab4_1/A.cs
+++ 20_Lab4_1/A.cs
@@ -3,10 +3,6 @@
 namespace _20_Lab4_1 {
 	class A {
 		public virtual void DebugInfo(params object[] values) {
-			// Якщо метод викликається об'єктом типу A,
-			// просто перелічити всі використовувані значення.
-			// А якщо метод викликається успадкованим від A об'єктом,
-			// перелічити тип і значення.
 			if (GetType().Equals(typeof(A))) {
 				for (int i = 0; i < values.Length; i++) {
 					Debug.WriteLine(values[i]);
